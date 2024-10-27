f34b54f6fcccf027a729587a47a251776ae364cd
Stiven Alvarez <90918269+INRIAS@users.noreply.github.com>
2024-10-26 23:29:45 -0500

    Add files via upload

---
+++ src/GenerarArchivo.java
@@ -0,0 +1,18 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import modelo.MonedaConstructor;
+import record.Moneda;
+
+public class GenerarArchivo {
+    public void generadorJson(MonedaConstructor moneda) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        FileWriter escritura = new FileWriter("Conversiones" + ".json");
+        escritura.write(gson.toJson(moneda));
+        escritura.close();
+    }
+}
+
